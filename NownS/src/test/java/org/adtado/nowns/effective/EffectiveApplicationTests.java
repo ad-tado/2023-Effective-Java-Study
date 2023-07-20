b3922629de485e5a713e2dc358acb50ab3a9c1c6
bc-kim <bc.kim@navercorp.com>
2023-07-21 01:40:40 +0900

    Initial Commit

---
+++ NownS/src/test/java/org/adtado/nowns/effective/EffectiveApplicationTests.java
@@ -0,0 +1,13 @@
+package org.adtado.nowns.effective;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EffectiveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ VSFe/build.gradle.kts
@@ -0,0 +1,25 @@
+plugins {
+	java
+	id("org.springframework.boot") version "3.1.2"
+	id("io.spring.dependency-management") version "1.1.2"
+}
+
+group = "org.adtado.vsfe"
+version = "0.0.1-SNAPSHOT"
+
+java {
+	sourceCompatibility = JavaVersion.VERSION_20
+}
+
+repositories {
+	mavenCentral()
+}
+
+dependencies {
+	implementation("org.springframework.boot:spring-boot-starter")
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
